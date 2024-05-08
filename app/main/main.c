3a4f90c7d792cfe744412a2f27cbda6de1a4eca7
AdrianoRean <127209495+AdrianoRean@users.noreply.github.com>
2024-05-08 10:32:54 +0200

    receive

--- app/main/main.c
+++ app/main/main.c
@@ -15,9 +15,29 @@ void task_tx(void *p)
    }
 }
 
+uint8_t buf[32];
+
+void receive_tx(void *p)
+{
+   int x;
+   for(;;) {
+    
+        printf("Trying to receive...");
+      lora_receive();    // put into receive mode
+      while(lora_received()) {
+         x = lora_receive_packet(buf, sizeof(buf));
+         buf[x] = 0;
+         printf("Received: %s\n", buf);
+         lora_receive();
+      }
+      vTaskDelay(1);
+   }
+}
+
 void app_main(void){
-    if(lora_init()==1)printf("Lora setup should be ok");
+    if(lora_init()==1)printf("Lora setup should be ok \n");
     lora_set_frequency(868e6);
     lora_enable_crc();
-    xTaskCreate(&task_tx, "task_tx", 2048, NULL, 5, NULL);
+    //xTaskCreate(&task_tx, "task_tx", 2048, NULL, 5, NULL);
+    xTaskCreate(&receive_tx, "receive_tx", 2048, NULL, 5, NULL);
 }
