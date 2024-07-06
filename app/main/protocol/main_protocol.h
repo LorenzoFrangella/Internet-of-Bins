8404406da4ae2c394ff66d702cbbcd387138f909
LorenzoFrangella <lorenzofrangellalollo@gmail.com>
2024-07-06 15:25:16 +0200

    Revert "Merge branch 'protocol-mod' of https://github.com/LorenzoFrangella/Internet-of-Bins into protocol-mod"
    This reverts commit ff860f058ad25c5a2624a7589aa8abd7ed5d90af, reversing
    changes made to 3249185cfba2a68d785329e1926687528842a690.

--- app/main/protocol/main_protocol.h
+++ app/main/protocol/main_protocol.h
@@ -5,22 +5,14 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <sys/time.h>
-#include <time.h>
 #include <string.h>
 #include "esp_log.h"
 #include <esp_random.h>
-#include "sensors.c"
 
 #include "ds3231.c"
 
 int gateway = 0;
 int id;
 int wifi = 0;
-
-monitor_task_parameters* parameters_monitor;
-
-#define MISSING_HOUR_LIMIT 2
-#define MAX_LEVEL 3
-#define WINDOW_SIZE 5
 
 #endif
--- app/main/protocol/protocol.c
+++ app/main/protocol/protocol.c
@@ -5,6 +5,8 @@
 #include "esp_wifi.h"
 #include "signature.c"
 
+
+
 #define MISSING_HOUR_LIMIT 2
 #define MAX_LEVEL 3
 #define WINDOW_SIZE 5
@@ -327,8 +329,6 @@ void protocol(void *pvParameters){
             
             ESP_LOGI(PROTOCOL_TAG, "Sending Data");
             time(&raw_time);
-
-            monitor_sensors(parameters_monitor);
             
             protocol_message message = generate_message(next_round);
             
